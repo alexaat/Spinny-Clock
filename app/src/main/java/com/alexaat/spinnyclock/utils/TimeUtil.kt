3cbdc6eea85c5a170153742576d5b0bd8080c082
AlexAAT <alexaat@yahoo.co.uk>
2020-07-24 19:24:06 +0100

    remove unnecessary imports

--- app/src/main/java/com/alexaat/spinnyclock/utils/TimeUtil.kt
+++ app/src/main/java/com/alexaat/spinnyclock/utils/TimeUtil.kt
@@ -1,11 +1,6 @@
 package com.alexaat.spinnyclock.utils
 
-import android.util.Log
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
-import kotlin.time.hours
-
 
 fun addMinute(hours:Int,minutes:Int):Calendar{
 
